64394136455353d69e24d0ec376770d7a78f7881
Adnan Niaz <adnanniaz77@yahoo.com>
2020-10-14 19:54:49 -0700

    added and styled a player model

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
-  let singer = "beyonce";
+  $('.player-wrapper').hide();
+
+  let singer = "";
   const limit = 30;
+  let current = {}
 
   $("form").on("submit", function (e) {
     e.preventDefault();
@@ -9,7 +12,7 @@ $(document).ready(function () {
 
     const searchValue = $("#search").val();
 
-    if (!searchValue) alert("Please enter Atrist name");
+    if (!searchValue) alert("Please enter Artist name");
 
     singer = searchValue;
 
@@ -34,18 +37,49 @@ $(document).ready(function () {
     result = await result.json();
     result = result.results;
 
-    console.log(result);
+    // console.log(result);
 
-    result.forEach((each) => {
-      const { artistName, artworkUrl100, previewUrl, trackName } = { ...each };
+    result.forEach((res) => {
+      const { 
+        artistName, 
+        artworkUrl100, 
+        previewUrl, 
+        trackName, 
+        collectionName 
+      } = { ...res };
       $(".container").append(`
-        <div class="box">
-            <img src=${artworkUrl100} alt=${artworkUrl100} />
-            <span class="singer">${artistName}</span>
-            <span class="song">${trackName}</span>
-            <audio src=${previewUrl} controls></audio>
+        <div class="track-link" data-preview=${previewUrl} 
+                                data-artist=${artistName} 
+                                data-trackName=${trackName} 
+                                data-poster=${artworkUrl100}>
+          <div class="box">
+              <img src=${artworkUrl100} alt=${artworkUrl100} />
+              <span class="singer">${artistName}</span>
+              <span class="song">${trackName}</span>
+              <span class="song">${collectionName}</span>
+              <!-- <audio src=${previewUrl} controls></audio> -->
+          </div>
         </div>
       `);
     });
   };
+
+  // click event to display player and play audio
+
+  $(document).on('click', '.track-link', function () {
+    console.log(this);
+
+    current["preview"] = $(this).data('preview');
+    current["artist"] = $(this).data('artist');
+    current["poster"] = $(this).data('poster');
+
+    console.log(current)
+
+    // player
+
+    $('.player-wrapper').show();
+    $('.artist').text(current.artist);
+    $('.disc').attr('src', current.poster);
+    $('#audio').attr('src', current.preview)
+  });
 });
