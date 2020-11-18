598b1c55efb8d545370080a6c96f5199286bd128
Adnan Niaz <adnanniaz77@yahoo.com>
2020-11-18 00:14:06 -0800

    saving changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,92 +1,101 @@
 $(document).ready(function () {
-  $('.player-wrapper').hide();
-
-  let singer = "";
-  const limit = 30;
-  let current = {}
-
-  $("form").on("submit", function (e) {
-    e.preventDefault();
-
-    $(".container").empty();
-
-    const searchValue = $("#search").val();
-
-    if (!searchValue) alert("Please enter Artist name");
-
-    singer = searchValue;
-
-    // fetch the results
-    fetchData();
-
-    // clear the form input
-    $("#search").val("");
-  });
-
-  // fetch results function
-  const fetchData = async function () {
-    let result = await fetch(
-      `https://itunes.apple.com/search?term=${singer}&limit=${limit}`
-    );
-    result = await result.json();
-    result = result.results;
-
-    // console.log(result);
-
-    result.forEach((res) => {
-      const { 
-        artistName, 
-        artworkUrl100, 
-        previewUrl, 
-        trackName, 
-        collectionName 
-      } = { ...res };
-      $(".container").append(`
-        <div class="track-link" data-preview=${previewUrl} 
-                                data-artist=${artistName} 
-                                data-trackName=${trackName} 
-                                data-poster=${artworkUrl100}>
-          <div class="box">
-              <img src=${artworkUrl100} alt=${artworkUrl100} />
+    let singer = "beyonce";
+    const limit = 30;
+  
+    $("form").on("submit", function (e) {
+      e.preventDefault();
+  
+      $(".container").empty();
+  
+      const searchValue = $("#search").val();
+  
+      if (!searchValue) alert("Please enter Atrist name");
+  
+      singer = searchValue;
+  
+      // fetch the results
+      fetchData();
+  
+      // clear the form input
+      $("#search").val("");
+    });
+  
+    // fetch results function
+    const fetchData = async function () {
+      let result = await fetch(
+        `https://itunes.apple.com/search?term=${singer}&limit=${limit}`
+      );
+      result = await result.json();
+      result = result.results;
+  
+      console.log(result);
+  
+      result.forEach((each) => {
+        const { artistName, artworkUrl100, previewUrl, trackName } = { ...each };
+        $(".container").append(`
+          <div class="box"
+                    data-artistName=${artistName} 
+                    data-trackName=${trackName}
+                    data-artworkUrl100=${artworkUrl100}
+                    data-previewUrl=${previewUrl}
+                    onclick="playThis(
+                        this.dataset.artistname, 
+                        this.dataset.artworkurl100, 
+                        this.dataset.previewurl, 
+                        this.dataset.trackname            
+                      )">
+              <!--<img src=${artworkUrl100} alt=${artworkUrl100}" />-->
               <span class="singer">${artistName}</span>
               <span class="song">${trackName}</span>
-              <!-- <span class="song">${collectionName}</span> -->
-              <audio src=${previewUrl} controls></audio> 
+              <!--<audio src=${previewUrl} controls onpause="pauseScript()" 
+                onplaying="myScript()"></audio>-->
           </div>
-        </div>
-      `);
-    });
-  };
+        `);
+      });
+    };
+  });
 
-  // click event to display player and play audio
+// click event to display player and play audio
 
   $(document).on('click', '.track-link', function () {
-    console.log(this);
-
     current["preview"] = $(this).data('preview');
     current["artist"] = $(this).data('artist');
     current["poster"] = $(this).data('poster');
-
-    console.log(current)
-
-    // player
-
-    $('.player-wrapper').show();
-    $('.artist').text(current.artist);
-    $('.disc').attr('src', current.poster);
-    $('#audio').attr('src', current.preview)
   });
-});
+  
 
-function myScript() {
-  const img = document.querySelector(`.box img`)
-  // console.log(img[0])
+const player = document.querySelector('.player')
+const songName = document.querySelector('.songName')
+const artist = document.querySelector('.artist')
+const disc = document.querySelector('.disc')
+const audioSrc = document.querySelector('.audioSrc')
 
-  img.classList.add('animate')
-  const tt = img.parentNode[2];
-  console.log(tt)
+function playThis(artistname, artworkurl100, previewurl, trackname) {
+  
+  document.querySelector('.player').style.display = 'block'
+  document.querySelector('.container').style.display = 'none'
+
+  artist.textContent = artistname 
+  songName.textContent = trackname 
+  disc.setAttribute('src', artworkurl100)
+  audioSrc.setAttribute('src', previewurl)
+  audioSrc.setAttribute('data-discImg', artworkurl100)
 }
 
-function pauseScript() {
-  document.querySelector('.box img').classList.remove('animate')
+function handlePlaying() {
+  disc.classList.add('animate')
 }
+
+function handlePause() {
+  disc.classList.remove('animate')
+}
+
+//////////////////////
+document.addEventListener('click', (e) => {
+  console.log(e.target)
+
+  if (e.target === document.body) {
+    document.querySelector('.player').style.display = 'none'
+    document.querySelector('.container').style.display = 'block'
+  }
+})
